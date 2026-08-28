496ab77bf54f9175b26f8de4b30072d4b2f7ab49
Ricky Kaare Engelharth <ricky@ricky.dk>
2026-08-28 16:26:27 +0200

    Upgrade test dependencies and move to MTP

--- test/Cabazure.Client.IntegrationTests/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using AutoFixture.AutoNSubstitute;
-global using AutoFixture.Xunit3;
-global using Cabazure.Test;
-global using FluentAssertions;
-global using NSubstitute;
--- test/Cabazure.Client.Runtime.Tests/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using AutoFixture.Xunit3;
-global using Cabazure.Test;
-global using FluentAssertions;
-global using NSubstitute;
